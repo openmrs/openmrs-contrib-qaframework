4e44f6f99482aae4626bad7c0dd7f93ba3f3c3fd
k-joseph <kaweesi@jembi.org>
2020-09-23 21:47:22 +0300

    disabled dryRun and refactored feature files

--- src/test/java/org/openmrs/contrib/qaframework/RunTest.java
+++ src/test/java/org/openmrs/contrib/qaframework/RunTest.java
@@ -6,6 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", glue = "org.openmrs.contrib.qaframework.automation", plugin = {
-		"html:target/index.html", "message:target/cucumber.ndjson"}, monochrome = true, tags = "@run", dryRun = true)
+		"html:target/index.html", "message:target/cucumber.ndjson"}, monochrome = true, tags = "@run")
 public class RunTest {
 }

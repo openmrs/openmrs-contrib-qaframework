f9d076446d3621240b4c1277266be482d6e1ee87
achilep <pahonsibebeto@gmail.com>
2020-10-16 13:34:14 +0100

    RATEST-68 Write attachments steps

---
+++ src/test/java/org/openmrs/contrib/qaframework/automation/AttachmentsSteps.java
@@ -0,0 +1,34 @@
+package org.openmrs.contrib.qaframework.automation;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class AttachmentsSteps extends Steps {
+
+    @Given ("system launches attachments page")
+    public void lunchAttachementPage(){
+
+    }
+
+    @And("user clicks in dropzone  or drops a file in the drop zone")
+    public void dropAFile(){
+
+    }
+
+    @And("user adds cation")
+    public void setCapyion(){
+
+    }
+
+    @And("clicks upload file")
+    public void uploadfile(){
+
+    }
+
+    @Then("then the file is uploaded")
+    public void checkAttachementList(){
+
+    }
+
+}

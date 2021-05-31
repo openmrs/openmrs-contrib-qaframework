7f0e5631c6604762b2001e733d4c387d527b3b1c
sherrif10 <sharfspace@gmail.com>
2021-05-31 20:21:11 +0300

    a Feature FIle For EditPatientRelationship and steps

---
+++ src/test/java/org/openmrs/contrib/qaframework/automation/EditPatientRelationshipSteps.java
@@ -0,0 +1,53 @@
+package org.openmrs.contrib.qaframework.automation;
+
+import org.openmrs.reference.page.ClinicianFacingPatientDashboardPage;
+import org.openmrs.reference.page.FindPatientPage;
+
+import org.openmrs.contrib.qaframework.RunTest;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EditPatientRelationshipSteps extends Steps {
+	private FindPatientPage findpatientPage;
+
+	private ClinicianFacingPatientDashboardPage clinicianFacingPatientDashboardPage;
+
+	@Before(RunTest.HOOK.SELENIUM_LOGIN)
+	public void systemLogin() {
+		initiateWithLogin();
+	}
+
+	@After(RunTest.HOOK.SELENIUM_DASHBOARD)
+	public void destroy() {
+		quit();
+	}
+
+	@Given("System loads clinicianFacingPatientDashboardPage")
+	public void loadClinicianFacingPatientDashboardPage() throws Exception {
+
+ 
+	}
+	@When("User clicks on editRegistrationInformation")
+	public void clickEditRegistrationInformation() {
+
+	}
+	@And("User clicks on EditRelationships")
+	public void clickEditRelationships() {
+
+	}
+
+	@And("User clicks on   Relationshiptype")
+	public void findRelationshipType() {
+ 
+	}
+	@Then("system loads back to patientDashboard")
+	public void loadPatientDashboard() {
+
+	}
+
+}
+
---
+++ src/test/resources/features/refapp-2.x/03-trackPatient/EditPatientRelationship.feature
@@ -0,0 +1,13 @@
+ Feature: EditRelationshipType
+ 
+ Given System loads clinicianFacingPatientDashboardPage
+    When  User clicks on editRegistrationInformation
+    And   User clicks on EditRelationships
+    And  User click on find Relationships
+    Then system loads back   to patientDashboard
+
+    Examples:
+      | RelationshipType |
+      | Doctor           |
+      | Nurse            |
+

5ac955200656b1ce1681197c2e55bb4aeb9fdccb
jayasanka-sack <jayasanka.sack@gmail.com>
2021-06-17 19:36:37 +0530

    MF-532: Implement an E2E test for the login flow of refapp-3.x

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,6 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
 var propertiesReader = require('properties-reader');
-var testProperties = propertiesReader('src/test/resources/org/openmrs/uitestframework/test.properties');
+var testProperties = propertiesReader('src/test/resources/org/openmrs/uitestframework/test-cypress.properties');
 
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber());

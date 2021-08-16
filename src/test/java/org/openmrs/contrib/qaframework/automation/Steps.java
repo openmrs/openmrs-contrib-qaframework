23c73367826cc8fc8d965466e041dff72ed6c916
MAGEMBE SHARIF <48015736+sherrif10@users.noreply.github.com>
2021-08-16 10:06:37 +0300

    Revert "RATEST-150: E2E Search and Registration of New Patient"

--- src/test/java/org/openmrs/contrib/qaframework/automation/Steps.java
+++ src/test/java/org/openmrs/contrib/qaframework/automation/Steps.java
@@ -1,25 +1,21 @@
 package org.openmrs.contrib.qaframework.automation;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.openmrs.reference.ReferenceApplicationTestBase;
 import org.openmrs.reference.page.ClinicianFacingPatientDashboardPage;
 import org.openmrs.reference.page.FindPatientPage;
 import org.openmrs.reference.page.HomePage;
 import org.openmrs.reference.page.PatientVisitsDashboardPage;
-import org.openmrs.reference.page.RegistrationPage;
-
 import org.openmrs.uitestframework.page.InitialSetupPage;
 import org.openmrs.uitestframework.page.LoginPage;
 import org.openmrs.uitestframework.page.TestProperties;
-import org.openmrs.uitestframework.test.TestData.TestPatient;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 // Use english locale, of not set, the test instance should be set to english language
 public class Steps extends ReferenceApplicationTestBase {
@@ -28,24 +24,10 @@ public class Steps extends ReferenceApplicationTestBase {
 	protected LoginPage loginPage;
 	protected FindPatientPage findPatientPage;
 	protected String firstPatientIdentifier;
-	TestPatient patient;
-	protected RegistrationPage registrationPage;
-	public PatientVisitsDashboardPage patientVisitsDashboardPage;
 	protected ClinicianFacingPatientDashboardPage dashboardPage;
 	protected By patientHeaderId = By.cssSelector("div.identifiers span");
 	protected InitialSetupPage initialSetupPage;
-	private String familyName = "Origin";
-	private String givenName = "Ashaba";
-	private String gender = "Female";
-	private String phoneNumber = "++21134567810";
-	private String estimatedYears = "24";
-	private String address1 = "Kenya";
-	private String villageName = "mwisho";
-	private String stateName = "Nairobi";
-	private String countryName = "Kenyata";
-	private String postalCode = "10101";
 	protected PatientVisitsDashboardPage visitsDashboardPage;
-
 
 	public Steps() {
 		try {
@@ -109,46 +91,4 @@ public class Steps extends ReferenceApplicationTestBase {
 		});
 		assertTrue(ids.contains(trimPatientId(patientId)));
 	}
-	public void systemLogin() {
-		goToLoginPage();
-		loginPage = getLoginPage();
-		loginPage.login("clerk", "Clerk123", "Registration Desk");
-	}
-
-	public void goToRegistrationApp() throws InterruptedException {
-		homePage = new HomePage(loginPage);
-		registrationPage = (RegistrationPage) homePage.goToRegisterPatientApp()
-				.waitForPage();
-	}
-
-	public void goToFindPatientRecordApp() throws InterruptedException {
-		findPatientPage = (FindPatientPage) homePage.goToFindPatientRecord()
-				.waitForPage();
-	}
-	public void goToHomePage() {
-		homePage.go();
-	}
-
-	public void searchForNewPatient() {
-		findPatientPage.enterPatient("Origin Ashaba");
-	}
-
-	public void enterPatientDetails() throws InterruptedException {
-		registrationPage.enterPatientFamilyName(familyName);
-		registrationPage.enterPatientGivenName(givenName);
-		registrationPage.clickOnGenderLink();
-		registrationPage.selectPatientGender(gender);
-		registrationPage.clickOnBirthDateLink();
-		registrationPage.enterEstimatedYears(estimatedYears);
-		registrationPage.clickOnContactInfo();
-		registrationPage.enterAddress1(address1);
-		registrationPage.enterVillage(villageName);
-		registrationPage.enterState(stateName);
-		registrationPage.enterCountry(countryName);
-		registrationPage.enterPostalCode(postalCode);
-		registrationPage.clickOnConfirmSection();
-		registrationPage.clickOnConfirmPatient();
-		registrationPage.waitForPage();
-	}
-
 }

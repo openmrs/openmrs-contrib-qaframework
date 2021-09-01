cd9ffbf23869d1a1d25d484194a7dfe5ba1d1e5c
pasindur99 <pasindur2pj@gmail.com>
2021-09-02 00:48:47 +0530

    RATEST-211: Fix formatting issue

--- cypress/integration/cucumber/step_definitions/refapp-3.x/02-search-and-registration/search-and-registration.js
+++ cypress/integration/cucumber/step_definitions/refapp-3.x/02-search-and-registration/search-and-registration.js
@@ -8,7 +8,7 @@ Given('the user login to the Registration Desk', () => {
 When('the user search for {string}', patientName => {
     cy.get('button[name=SearchPatientIcon]').click();
     cy.getByPlaceholder('Search for a patient by name or identifier number').type(patientName);
-//Adding an artificial wait because it takes time to get typed test updates
+    // Adding an artificial wait because it takes time to get typed text updates
     cy.wait(500);
     cy.contains('Search').click({force:true});
 })

b89489b1001d79860e41a2ee8b83a20a4ab86573
sherrif10 <sharfspace@gmail.com>
2021-10-13 21:16:40 +0300

    adding header message on every page class

--- src/test/java/org/openmrs/contrib/qaframework/page/ActiveVisitsPage.java
+++ src/test/java/org/openmrs/contrib/qaframework/page/ActiveVisitsPage.java
@@ -13,6 +13,16 @@ import org.openmrs.contrib.qaframework.helper.Page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ * 
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+
 public class ActiveVisitsPage extends Page {
 
 	private static final By ACTIVE_PATIENT = By.xpath("//td[2]/a");

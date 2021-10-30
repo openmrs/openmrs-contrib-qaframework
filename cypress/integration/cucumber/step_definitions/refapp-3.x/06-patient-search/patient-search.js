20fd0d2371c7dfcbc006037199065840d5bf47e6
pasindur2 <pasindur2pj@gmail.com>
2021-10-30 10:20:23 +0530

    Fix failing patient-search 3.X E2E test

--- cypress/integration/cucumber/step_definitions/refapp-3.x/06-patient-search/patient-search.js
+++ cypress/integration/cucumber/step_definitions/refapp-3.x/06-patient-search/patient-search.js
@@ -13,7 +13,7 @@ When('the user search for {string}', patientName => {
     cy.get('button[name=SearchPatientIcon]').click();
     cy.getByPlaceholder('Search for a patient by name or identifier number').type(patientName);
     // Adding an artificial wait because it takes time to get typed text updates
-    cy.wait(500);
+    cy.wait(1000);
     cy.contains('Search').click({force:true});
 })
 

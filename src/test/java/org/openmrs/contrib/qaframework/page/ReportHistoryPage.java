55efb376e1283f569eed657e8a9f5c11b62b8083
sherrif10 <sharfspace@gmail.com>
2021-11-05 09:10:51 +0300

    RATEST-266: Fix Report workflow

--- src/test/java/org/openmrs/contrib/qaframework/page/ReportHistoryPage.java
+++ src/test/java/org/openmrs/contrib/qaframework/page/ReportHistoryPage.java
@@ -11,20 +11,30 @@ package org.openmrs.contrib.qaframework.page;
 
 import org.openmrs.contrib.qaframework.helper.Page;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class ReportHistoryPage extends Page {
-
+	
 	private static final By VIEW_REPORT = By.cssSelector("#content a[href*='/module/reporting/reports/viewReport.form']");
-
+	private static final By ERROR_DETAILS = By.cssSelector("#errorDetailsLink");
+	private static final By VIEW_LOG = By.cssSelector("#viewReportLogLink");
+	
 	public ReportHistoryPage(Page page) {
 		super(page);
 	}
-
+	
 	public RenderDefaultReportPage clickOnViewLink() {
+		if (driver.findElement(ERROR_DETAILS).isDisplayed()) {
+			clickOn(VIEW_LOG);
+		}
+		else {
+		waiter.until(ExpectedConditions.visibilityOfElementLocated(VIEW_REPORT));
 		clickOn(VIEW_REPORT);
 		return new RenderDefaultReportPage(this);
+		}
+	   return new RenderDefaultReportPage(this);
 	}
-
+	
 	@Override
 	public String getPageUrl() {
 		return "/reporting/reports/reportHistoryOpen";

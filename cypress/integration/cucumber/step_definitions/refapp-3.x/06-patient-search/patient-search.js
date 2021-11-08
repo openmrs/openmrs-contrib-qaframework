10b1f6002fe3b5bb24409d647e5559ca7f5d8368
pasindur2 <pasindur2pj@gmail.com>
2021-11-08 22:56:12 +0530

    FIx search workflow error

--- cypress/integration/cucumber/step_definitions/refapp-3.x/06-patient-search/patient-search.js
+++ cypress/integration/cucumber/step_definitions/refapp-3.x/06-patient-search/patient-search.js
@@ -12,9 +12,9 @@ Given('the user login to the Registration Desk', () => {
 When('the user search for {string}', patientName => {
     cy.get('button[name=SearchPatientIcon]').click();
     cy.getByPlaceholder('Search for a patient by name or identifier number').type(patientName);
-    // Adding an artificial wait because it takes time to get typed text updates
+    // Adding an artificial wait because typed texts takes some time to get updated
     cy.wait(1000);
-    cy.contains('Search').click({force:true});
+    cy.getByPlaceholder('Search for a patient by name or identifier number').type('{enter}');
 })
 
 Then('the result should be {string}', result => {

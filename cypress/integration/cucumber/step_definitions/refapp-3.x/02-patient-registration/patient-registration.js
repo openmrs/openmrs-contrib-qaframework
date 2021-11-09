e1f9308187361b095d4fd7a078951e74f9fb0ce3
Pasindu Rupasinghe <77311602+pasindur99@users.noreply.github.com>
2021-11-09 10:16:14 +0530

    MF-900: Create a separate workflow for search E2E tests (#200)
    * MF-900: Create a separate workflow for search E2E tests
    
    * Rename files to be more readable
    
    * Fix failing patient-search 3.X E2E test
    
    * FIx search workflow error

--- cypress/integration/cucumber/step_definitions/refapp-3.x/02-patient-registration/patient-registration.js
+++ cypress/integration/cucumber/step_definitions/refapp-3.x/02-patient-registration/patient-registration.js
@@ -9,19 +9,6 @@ Given('the user login to the Registration Desk', () => {
     cy.visit('home');
 })
 
-When('the user search for {string}', patientName => {
-    cy.get('button[name=SearchPatientIcon]').click();
-    cy.getByPlaceholder('Search for a patient by name or identifier number').type(patientName);
-    // Adding an artificial wait because it takes time to get typed text updates
-    cy.wait(500);
-    cy.contains('Search').click({force:true});
-})
-
-Then('the result should be {string}', result => {
-    cy.contains(result);
-});
-
-
 When('the user clicks on the add patient icon', () => {
     cy.get('button[name=SearchPatientIcon]');
     cy.get('button[name=AddPatientIcon]').click();

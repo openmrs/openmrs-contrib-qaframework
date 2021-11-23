5abb5fe05cb2859c7dc5decef5d8cd01dfaa362b
hadijahkyampeire <hadijah315@gmail.com>
2021-11-23 21:57:01 +0300

    Add credentials to cypress env

--- cypress/integration/cucumber/step_definitions/oclsubscriptionmodule/index.js
+++ cypress/integration/cucumber/step_definitions/oclsubscriptionmodule/index.js
@@ -1,7 +1,13 @@
 import { Given, Then, When } from 'cypress-cucumber-preprocessor/steps';
+import { 
+  OCL_USER_NAME, 
+  OCL_USER_PASSWORD, 
+  OCL_USER_TOKEN,
+  OPENMRS_USER_NAME, 
+  OPENMRS_USER_PASSWORD
+} from '../../../../support/commands';
 
-const userApiToken = 'e6415c890158e06c36f7f52eca335bdf7204d50f'; //TODO: Fetch from the API and load directly
-const sUrl = 'https://api.staging.openconceptlab.org/users/openmrs/collections/TCM/1/';
+const subscriptionUrl = 'https://api.staging.openconceptlab.org/users/openmrs/collections/TCM/1/';
 
 const fetchData = (url) => fetch(url)
   .then((response) => {
@@ -19,15 +25,15 @@ Given('the user goes to the openmrs application', () => {
 });
 
 When('the user logs into the Dictionary Manager', () => {
-  cy.get("#username").type('openmrs');
-  cy.get("#password").type('Openmrs123');
+  cy.get("#username").type(OCL_USER_NAME);
+  cy.get("#password").type(OCL_USER_PASSWORD);
   cy.get("#login-form form").submit();
   cy.url().should('contain', 'https://openmrs.staging.openconceptlab.org/user/collections/');
 });
 
 When('the user logs into the openmrs Ref App', () => {
-  cy.get("#username").type('admin');
-  cy.get("#password").type('Admin123');
+  cy.get("#username").type(OPENMRS_USER_NAME);
+  cy.get("#password").type(OPENMRS_USER_PASSWORD);
   cy.get("#Pharmacy").click();
   cy.get("#loginButton").click();
   cy.url().should('contain', 'http://localhost:8080/openmrs/referenceapplication/home.page')
@@ -59,8 +65,8 @@ When('the user visit the OCL module page', () => {
 
 When('the user pastes the subscription url and user API token', () => {
   // TODO: We need to unsubscribe at some point 
-  cy.get("#subscription-url").type(sUrl);
-  cy.get("#subscription-token").type(userApiToken);
+  cy.get("#subscription-url").type(subscriptionUrl);
+  cy.get("#subscription-token").type(OCL_USER_TOKEN);
 });
 
 When('the user saves changes', () => {

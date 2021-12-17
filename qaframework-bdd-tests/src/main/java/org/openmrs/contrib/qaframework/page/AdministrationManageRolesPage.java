eda56cf1ce1f64d15ada44fe7921ef6e31c0194c
mherman22 <hermanmuhereza22@gmail.com>
2021-12-17 15:24:45 +0300

    updating the edit role link in the administrationManageROlesPage

--- qaframework-bdd-tests/src/main/java/org/openmrs/contrib/qaframework/page/AdministrationManageRolesPage.java
+++ qaframework-bdd-tests/src/main/java/org/openmrs/contrib/qaframework/page/AdministrationManageRolesPage.java
@@ -16,9 +16,9 @@ public class AdministrationManageRolesPage extends Page{
 
 	protected static final String PAGE_URL = "/openmrs/admin/users/role.list";
 	private static final By ADD_NEW_ROLE_LINK= By.cssSelector("#content > a");
-	private static final By DELETE_SELECTED_ROLES_BUTTON = By.cssSelector("#content > form > input[type=submit]");
-	private static final By CHECKBOX = By.cssSelector("#content > form > table > tbody input[type=checkbox]");
-	private static final By EDIT_ROLE_LINK = By.cssSelector("#content form table tbody td:nth-child(2)");
+	private static final By DELETE_SELECTED_ROLES_BUTTON = By.cssSelector("#content form input[type=submit]");
+	private static final By CHECKBOX = By.cssSelector("#content form table tbody input[type=checkbox");
+	private static final By EDIT_ROLE_LINK = By.cssSelector("#content form table tbody tr:nth-child(3) td:nth-child(2) a");
 
 	public AdministrationManageRolesPage(Page parent) {
 		super(parent);

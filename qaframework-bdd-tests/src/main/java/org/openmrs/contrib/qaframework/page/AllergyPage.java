fb48d0181a9539f44732dd206d79d505a0a86cad
“irenyak1” <inyakate@gmail.com>
2022-01-04 13:48:55 +0300

    RATEST-286: Removes empty line

--- qaframework-bdd-tests/src/main/java/org/openmrs/contrib/qaframework/page/AllergyPage.java
+++ qaframework-bdd-tests/src/main/java/org/openmrs/contrib/qaframework/page/AllergyPage.java
@@ -24,7 +24,6 @@ public class AllergyPage extends Page {
 	private static final By ADD_NO_KNOWN = By.xpath("//*[contains(text(),'No Known Allergy')]");
 	private static final By DELETE_NO_KNOWN = By.className("delete-action");
 	private static final By RETURN_BUTTON = By.cssSelector("#content > button.cancel");
-	
 
 	public AllergyPage(Page page) {
 		super(page);

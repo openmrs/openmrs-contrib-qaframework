531757b47a13cafdbfc61039df7fdd851809d42a
sherrif10 <sharfspace@gmail.com>
2022-02-02 22:00:48 +0300

    update the test

--- qaframework-bdd-tests/src/test/java/org/openmrs/contrib/qaframework/automation/VerifyLoginSteps.java
+++ qaframework-bdd-tests/src/test/java/org/openmrs/contrib/qaframework/automation/VerifyLoginSteps.java
@@ -7,30 +7,22 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 import static org.junit.Assert.assertTrue;
-
 import org.openmrs.contrib.qaframework.RunTest;
-import org.openmrs.contrib.qaframework.page.HomePage;
 
 public class VerifyLoginSteps  extends Steps {
 
-	@Before(RunTest.HOOK.SELENIUM_DEFAULT_LOGIN)
-	public void systemLogin() {
-		initiateWithLogin();
-	}
-
-	@After(RunTest.HOOK.SELENIUM_DEFAULT_LOGIN)
-	public void destroy() {
-		quit();
-	}
-    
-    public void initiateHomePage(){
-        homePage = new HomePage(page);
-        assertPage(homePage.waitForPage());
+    @Before(RunTest.HOOK.SELENIUM_DEFAULT_LOGIN)
+    public void visitHomePage() {
+        initiateWithLogin();
     }
     
+    @After(RunTest.HOOK.SELENIUM_DEFAULT_LOGIN)
+    public void destroy() {
+
+    }
+
     @When("user verifies modules available on home page")
     public void verifyModulesAvailableOnHomePage() {
-        initiateHomePage();
         assertTrue(homePage.isFindAPatientAppPresent());
         assertTrue(homePage.isActiveVisitsAppPresent());
         assertTrue(homePage.isAppointmentSchedulingAppPresent());
@@ -44,7 +36,6 @@ public class VerifyLoginSteps  extends Steps {
     @And("user verifies modules available on homepage after login as clerk")
     public void verifyClerkModulesAvailableOnHomePage() {
         goToLoginPage().loginAsClerk();
-        initiateHomePage();
         assertTrue(homePage.isActiveVisitsAppPresent());
         assertTrue(homePage.isAppointmentSchedulingAppPresent());
         assertTrue(homePage.isRegisterPatientCustomizedForRefAppPresent());
@@ -53,7 +44,6 @@ public class VerifyLoginSteps  extends Steps {
     @And("user verifies modules available on home page after login as Docter")
     public void verifyDoctorModulesAvailableOnHomePage() {
         goToLoginPage().loginAsDoctor();
-        initiateHomePage();
         assertTrue(homePage.isFindAPatientAppPresent());
         assertTrue(homePage.isActiveVisitsAppPresent());
         assertTrue(homePage.isAppointmentSchedulingAppPresent());
@@ -62,7 +52,6 @@ public class VerifyLoginSteps  extends Steps {
     @And("user verifies modules available on home page after login as Nurse")
     public void verifyNurseModulesAvailableOnHomePage() {
         goToLoginPage().loginAsNurse();
-        initiateHomePage();
         assertTrue(homePage.isFindAPatientAppPresent());
         assertTrue(homePage.isActiveVisitsAppPresent());
         assertTrue(homePage.isAppointmentSchedulingAppPresent());
@@ -72,7 +61,6 @@ public class VerifyLoginSteps  extends Steps {
     @Then("user verifies modules available on home page after login as sysadmin")
     public void verifySysadminModulesAvailableOnHomePage() {
         goToLoginPage().loginAsSysadmin();
-        initiateHomePage();
         assertTrue(homePage.isAppointmentSchedulingAppPresent());
         assertTrue(homePage.isSystemAdministrationAppPresent());
     }

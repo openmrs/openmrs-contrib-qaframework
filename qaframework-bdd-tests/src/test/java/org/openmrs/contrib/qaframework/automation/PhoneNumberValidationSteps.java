95d599c86fd6ce0707724393c40fe55094dd5c21
sherrif10 <sharfspace@gmail.com>
2022-03-01 17:33:08 +0300

    use same formating instead of variable

--- qaframework-bdd-tests/src/test/java/org/openmrs/contrib/qaframework/automation/PhoneNumberValidationSteps.java
+++ qaframework-bdd-tests/src/test/java/org/openmrs/contrib/qaframework/automation/PhoneNumberValidationSteps.java
@@ -20,15 +20,12 @@ import io.cucumber.java.en.When;
 
 import org.openmrs.contrib.qaframework.RunTest;
 import org.openmrs.contrib.qaframework.helper.TestData;
-import org.openmrs.contrib.qaframework.page.ActiveVisitsPage;
 import org.openmrs.contrib.qaframework.page.RegistrationEditSectionPage;
 
 public class PhoneNumberValidationSteps  extends Steps {
     
     private TestData.PatientInfo patient;
-    private ActiveVisitsPage activeVisitsPage;
     private RegistrationEditSectionPage registrationEditSectionPage;
-    private static final String INVALID_PHONE_NUMBER = "abcdefgh1";
 
     @Before(RunTest.HOOK.SELENIUM_PHONE_NUMBER_VALIDATION)
     public void setUp() throws Exception {
@@ -43,33 +40,39 @@ public class PhoneNumberValidationSteps  extends Steps {
         quit();
     }  
     
-    @Given("user goes to active visit search page")
-    public void userGoesToActiveVisitSearch() {
-        activeVisitsPage = homePage.goToActiveVisitsSearch();
+    @Given("user goes to find patient record app")
+    public void userGoesToFindPatientApp() {
+        findPatientPage = homePage.goToFindPatientRecord();
     }
 
     @When("user searches for patient using patient identifier")
     public void searchForPatientUsingPatientIdentifier() {
-        activeVisitsPage.search(patient.identifier);
+        findPatientPage.enterPatient(patient.identifier);
+    	findPatientPage.waitForPageToLoad();
     }
 
-    @Then("system loads patient dashboard page of last active visit")
-    public void systemGoesToPatientDashboardOfLastActiveVisit() {
-        dashboardPage = activeVisitsPage.goToPatientDashboardOfLastActiveVisit();
+    @And("user clicks on the first patient")
+    public void userClicksOnFirstPatient() {
+        dashboardPage = findPatientPage.clickOnFirstPatient();
+    }
+
+    @Then("system loads patient dashboard page")
+    public void loadDashboardPage() {
+        assertPage(dashboardPage.waitForPage());
     }
     
     @And("user clicks on show contact link")
-    public void clickOnShowContact() {
+    public void userClicksOnShowContact() {
         dashboardPage.clickOnShowContact();
     }
 
     @And("user clicks on edit contact info link")
-    public void clickOnEditContact() {
+    public void userClickOnEditContact() {
         registrationEditSectionPage = dashboardPage.clickOnEditContact();
     }
 
-    @And("user clicks on phone number button")
-    public void userClicksOnPhoneNumber(){
+    @And("user clicks on phone number tab")
+    public void userClicksOnPhoneNumber() {
         registrationEditSectionPage.clickOnPhoneNumberEdit();
     }
 
@@ -79,8 +82,8 @@ public class PhoneNumberValidationSteps  extends Steps {
     }
 
     @And("user enters an invalid phone number")
-    public void userEntersPhoneNumber() {
-        registrationEditSectionPage.enterPhoneNumber(INVALID_PHONE_NUMBER);
+    public void userEntersInvalidPhoneNumber() {
+        registrationEditSectionPage.enterPhoneNumber("abcdefg1");
     }
 
     @And("user clicks on confirm edit button")
@@ -94,17 +97,17 @@ public class PhoneNumberValidationSteps  extends Steps {
     }
 
     @And("user clears the phone number input area")
-    public void userClearPhoneNumber() {
+    public void userClearsThePhoneNumber() {
         registrationEditSectionPage.clearPhoneNumber();
     }
 
     @And("user enters the correct phone number")
-    public void userReEntersCorrectPhoneNumber() {
+    public void userEntersCorrectPhoneNumber() {
         registrationEditSectionPage.enterPhoneNumber("111111111");
     }
 
     @And("user clicks again on confirm edit button")
-    public void userReClicksOnConfirmsEditButton() {
+    public void clicksOnConfirmEditButton() {
         registrationEditSectionPage.clickOnConfirmEdit();
     }
 
@@ -114,12 +117,12 @@ public class PhoneNumberValidationSteps  extends Steps {
     }
 
     @And("user clicks again on show contact link")
-    public void userReClicksOnShowContact() {
+    public void userClicksAgainOnShowContact() {
         dashboardPage.clickOnShowContact();
     }
 
     @Then("system validates the correct phone number to be entered into the input area")
-    public void systemChecksTheCollectPhoneNumber() {
+    public void systemChecksTheCorrectPhoneNumber() {
         assertTrue(dashboardPage.getTelephoneNumber().contains("111111111"));
     }
 }
--- qaframework-bdd-tests/src/test/resources/features/refapp-2.x/16-PhoneNumberValidation/phoneNumberValidation.feature
+++ qaframework-bdd-tests/src/test/resources/features/refapp-2.x/16-PhoneNumberValidation/phoneNumberValidation.feature
@@ -1,16 +1,17 @@
 Feature:Phone Number validation
 
   Background:
-    Given user goes to active visit search page
+    Given user goes to find patient record app
    
   @selenium
   @phoneNumberValidation
   Scenario: Phone Number validation
     When user searches for patient using patient identifier
-    Then system loads patient dashboard page of last active visit
+    And user clicks on the first patient
+    Then system loads patient dashboard page
     And user clicks on show contact link
     And user clicks on edit contact info link
-    And user clicks on phone number button
+    And user clicks on phone number tab
     And user clears phone number
     And user enters an invalid phone number
     And user clicks on confirm edit button

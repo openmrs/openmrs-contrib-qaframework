033e371ec4020f77bf991dbfc0601472cda9bd6d
“irenyak1” <inyakate@gmail.com>
2022-03-04 09:40:44 +0300

    RATEST-311: Change element selectors

--- qaframework-bdd-tests/src/main/java/org/openmrs/contrib/qaframework/page/EncounterFormPage.java
+++ qaframework-bdd-tests/src/main/java/org/openmrs/contrib/qaframework/page/EncounterFormPage.java
@@ -20,13 +20,13 @@ public class EncounterFormPage extends Page {
 	private static final String URL = "/admin/encounters/encounter.form";
 	private static final By PATIENT_FIELD= By.id("patientId_id_selection");
 	private static final By FIRST_AUTOCOMPLETE_RESULT = By.cssSelector("body ul li a span");
-	private static final By ENCOUNTER_DATE_TIME_FIELD= By.cssSelector("#content form div:nth-child(1) table tbody tr:nth-child(3) td input[type=text]");
+	private static final By ENCOUNTER_DATE_TIME_FIELD= By.xpath("//input[@name='encounterDatetime']");
 	private static final By ROLE_SELECT = By.id("roleIds[0]");
 	private static final By PROVIDER_NAME_FIELD = By.name("providerName");
 	private static final By SAVE_ENCOUNTER_BUTTON = By.id("saveEncounterButton");
 	private static final By DELETE_CHECKBOX = By.id("voided");
 	private static final By VOID_REASON_FIELD = By.name("voidReason");
-	private static final By CLOSE_CALENDER_BUTTON = By.cssSelector("#ui-datepicker-div > div.ui-datepicker-buttonpane.ui-widget-content > button.ui-datepicker-close.ui-state-default.ui-priority-primary.ui-corner-all");
+	private static final By CLOSE_CALENDER_BUTTON = By.xpath("//button[@class='ui-datepicker-close ui-state-default ui-priority-primary ui-corner-all']");
 
 	public EncounterFormPage(Page page) {
 		super(page);
@@ -46,7 +46,7 @@ public class EncounterFormPage extends Page {
 	}
 
 	public void closeCalenderWithEnterButton() {
-		WebElement dateTimeElement = driver.findElement(By.xpath("//*[@id=\"encounterDatetime\"]"));
+		WebElement dateTimeElement = driver.findElement(By.xpath("//input[@name='encounterDatetime']"));
 		dateTimeElement.sendKeys(Keys.RETURN);
 	}
 

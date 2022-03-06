c9cba37c59284621b8c11de50f0b097f5ae27a01
mherman22 <hermanmuhereza22@gmail.com>
2022-03-06 09:40:42 +0300

    changing the methods pointing to click on manage users link

--- qaframework-bdd-tests/src/test/java/org/openmrs/contrib/qaframework/automation/AddRoleToUserSteps.java
+++ qaframework-bdd-tests/src/test/java/org/openmrs/contrib/qaframework/automation/AddRoleToUserSteps.java
@@ -137,8 +137,7 @@ public class AddRoleToUserSteps extends Steps {
 	 @And("user logins into the system as an admin")
 	 public void loginIntoSystemAsAnAdmin() {
                  goToLoginPage().loginAsAdmin();
-                 homePage.goToAdministration();
-                 administrationPage.clickOnManageUsers();
+                 homePage.goToSystemAdministrationPage().goToAdvancedAdministration().clickOnManageUsers();
 	 }
 	 
 	 @And("user deletes the user")
